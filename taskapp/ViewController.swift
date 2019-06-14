89a4acaa98778ae9765bb3a5b8991ab44e1a4393
isobe <ngnp.pngn@icloud.com>
2019-06-15 01:24:31 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by be on 2019/06/15.
+//  Copyright © 2019年 isobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
